a0fc437766339bbbfa3ded3845c513cf2d21fee9
Brian Flores <brian.flores.ch@gmail.com>
2022-09-05 21:30:35 -0300

    Completo

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const PORT = 3000;
+const express = require('express')
+const app = express()
+const PORT = 3000
+const path = require('path')
 
 
-app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.static("public"))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/index.html"))
+    res.sendFile(path.join(__dirname, "views/home.html"))
 })
 
 
-app.listen(PORT, () => console.log(`Server ON ${PORT}\nLink: http://localhost:${PORT}`))
+app.listen(PORT, () => {
+    console.log(`Servidor levantado http://localhost:${PORT}`);
+})
